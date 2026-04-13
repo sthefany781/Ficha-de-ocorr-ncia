58ecca3e8592836b84a1ea96f12ba3a11d02da70
camila1234c <160602494+camila1234c@users.noreply.github.com>
2026-04-13 09:48:07 -0300

    Add functions to select, save, and clear data

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,39 @@
+let tipoSelecionado = "";
+
+
+function selecionarTipo(tipo) {
+    tipoSelecionado = tipo;
+
+    document.querySelector(".negativa").classList.remove("ativo");
+    document.querySelector(".positiva").classList.remove("ativo");
+
+    if (tipo === "negativa") {
+        document.querySelector(".negativa").classList.add("ativo");
+    } else {
+        document.querySelector(".positiva").classList.add("ativo");
+    }
+}
+
+function salvar() {
+    let nome = document.getElementById("nome").value;
+    let cgm = document.getElementById("cgm").value;
+
+    if (!nome || !cgm || !tipoSelecionado) {
+        alert("Preencha os campos obrigatórios!");
+        return;
+    }
+
+    alert("Ocorrência salva com sucesso!");
+}
+
+function limpar() {
+    document.getElementById("nome").value = "";
+    document.getElementById("cgm").value = "";
+    document.getElementById("descricao").value = "";
+
+    tipoSelecionado = "";
+
+    document.querySelector(".negativa").classList.remove("ativo");
+    document.querySelector(".positiva").classList.remove("ativo");
+}
 
